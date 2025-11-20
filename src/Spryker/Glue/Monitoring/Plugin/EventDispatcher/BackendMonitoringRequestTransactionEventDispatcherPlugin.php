24a538e8c3ba4e610e55328e2de8fed61889beca
Alexander Veselov <alexander.veselov@spryker.com>
2025-11-20 11:19:11 +0000

    FRW-9952 Created different monitor plugins for GLUE layer. (#89)
    FRW-9952 Created different monitor plugins for GLUE layer.

---
+++ src/Spryker/Glue/Monitoring/Plugin/EventDispatcher/BackendMonitoringRequestTransactionEventDispatcherPlugin.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\Monitoring\Plugin\EventDispatcher;
+
+/**
+ * @method \Spryker\Glue\Monitoring\MonitoringFactory getFactory()
+ */
+class BackendMonitoringRequestTransactionEventDispatcherPlugin extends AbstractMonitoringRequestTransactionEventDispatcherPlugin
+{
+    /**
+     * @return string
+     */
+    protected function getLocaleName(): string
+    {
+        return $this->getFactory()->getLocaleFacade()->getCurrentLocale()->getLocaleName();
+    }
+}
